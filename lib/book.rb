0de64157cb1d9f12162c65989ba525e615e4e20e
chrstno040420 <104917222+chrstno040420@users.noreply.github.com>
2022-07-24 18:28:07 -0500

    commit tree 857ad1b6721fef32d795abf23ad6cd78707abedf

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,25 @@
+require "pry"
+
+class Book
+
+attr_reader 
+ attr_accessor :title, :author, :page_count, :genre, :turn_page
+
+def initialize(title)
+    @title = title
+   
+    
+ 
+end
+
+def turn_page(turn_page)
+  @turn_page = turn_page + 1
+end
 
 
+end
+
+# book = Book.new("The World According to Garp" ,
+book = Book.new("And Then There Were None")
+
+#   binding.pry
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,14 @@
 # Make your shoe class here!
+require"pry"
+
+class Shoe
+
+attr_accessor  :brand, :color, :size, :material, :condition
+
+def initialize(shoe)
+@shoe = shoe
+end
+end
+
+binding.pry
+shoe= Shoe.new("Nike")
